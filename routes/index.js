ca1331051d19bd744d6bb86a2a88c07ca3e79d01
ISHWEENKHATRI <ishween.khatri.csai.20@ggits.net>
2024-02-11 19:08:19 +0530

    Full Code Pushed

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,34 @@
 const router=require("express").Router();
-
+const tasks = require("../models/tasks.js");
+const Task=require("../models/tasks.js");
 //routes
-router.get("/",(req,res)=>{
-    res.render("index");
+router.get("/", async (req,res)=>{
+    const allTasks=await Task.find();
+    res.render("index",{task:allTasks});
+})
+
+router.post("/",(req,res)=>
+{
+  const tasks=req.body.task;
+  const newTask= new Task({task:tasks});
+
+  newTask.save()
+  .then(()=>{
+    console.log("Task Added Successfully");
+    res.redirect("/");
+  })
+  .catch((err)=>console.log(err));
+})
+
+router.get("/delete/task/:_id",(req,res)=>{
+    const id=req.params._id;
+    Task.deleteOne({_id:id})
+    .then(()=>{
+        console.log("Task Deleted Successfully");
+        res.redirect("/");
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
 })
 module.exports=router;
